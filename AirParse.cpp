1f78e3d6a5ba65dc72ececf30499e6211e61ebeb
JasonTheCPE <jasonthecpe@gmail.com>
2016-02-08 12:06:00 -0800

    Update AirParse.cpp

--- AirParse.cpp
+++ AirParse.cpp
@@ -1,1 +1,38 @@
 
+#include <stdio.h>
+#include <fstream.h>
+#include <iostream.h>
+#include <string.h>
+#include "AirParse.h"
+
+using namespace std;
+
+#define ID_INDEX = 0
+#define CITYNAME_INDEX = 2
+#define ALIAS = 4
+#define LAT = 6
+#define LON = 7
+
+void ParseAirports(string filename, airport_base *ab) {
+   ifstream iFile(filename);
+   char *line;
+   char *separators = ",";
+   char *tokens;
+   int index;
+
+   while(getline(iFile, line)) {
+      tokens = (line, separators);
+      index = 0;
+      while(index tokens != NULL) {
+         if() {
+            
+         }
+      }
+   }
+   
+}
+
+
+void ParseRoutes(string filename, route_table *rt) {
+
+}
