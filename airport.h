940f4fb2c9bf569c7b1225ae76ac7f43561852b6
JasonTheCPE <jasonthecpe@gmail.com>
2016-02-09 08:22:48 -0800

    fixed some compilation issues

--- airport.h
+++ airport.h
@@ -1,20 +1,23 @@
 #ifndef _AIRPORT_BASE_
 #define _AIRPORT_BASE_
 
+#include <vector>
+#include <string>
+
 class airport_base
 {
 public:
     airport_base();
-    airport_base::airport_base(int size)
+    airport_base::airport_base(int size);
     ~airport_base();
-    
+
     int numAirports;
-    std::vector<int> ids;
-    std::vector<string> cities;
-    std::vector<string> names;      // can throw out later
-    std::vector<string> alias;
-    std::vector<double> lats;
-    std::vector<double> longs;
+    std::vector<int> ids;        //Ex: 5768
+    std::vector<std::string> cities;  //Ex: San Luis Obispo
+    //std::vector<string> names;   //Ex: ???
+    std::vector<std::string> alias;   //Ex: SBP
+    std::vector<double> lats;    //Ex: 35.2368
+    std::vector<double> longs;   //Ex: -120.624
 
     double get_distance(int a_Id, int b_Id);
 };
