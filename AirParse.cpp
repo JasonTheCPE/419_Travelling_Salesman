bfe4099dc88d8a84438794a94890af453cb61a8e
JasonTheCPE <jasonthecpe@gmail.com>
2016-02-09 08:25:12 -0800

    Update AirParse.cpp

--- AirParse.cpp
+++ AirParse.cpp
@@ -1,38 +1,62 @@
 
-#include <stdio.h>
-#include <fstream.h>
-#include <iostream.h>
-#include <string.h>
 #include "AirParse.h"
 
 using namespace std;
 
-#define ID_INDEX = 0
-#define CITYNAME_INDEX = 2
-#define ALIAS = 4
-#define LAT = 6
-#define LON = 7
-
-void ParseAirports(string filename, airport_base *ab) {
-   ifstream iFile(filename);
-   char *line;
-   char *separators = ",";
-   char *tokens;
+int ParseAirports(char *filename, airport_base *ab) {
+   ifstream inFile(filename);
+   stringstream ss;
+   string line;
+   string delimiters = ",\"";
+   int numAirports = 0;
    int index;
+   size_t prev, pos;
 
-   while(getline(iFile, line)) {
-      tokens = (line, separators);
-      index = 0;
-      while(index tokens != NULL) {
-         if() {
-            
+/* //I wish we could use thee regular expression, sadly tis not to be *sadness*
+   regex regEx("^(\\d{1,}),\"([a-zA-Z ]{1,})\".*?\"([a-zA-Z]{3})\",.*?,([0-9\\-\\.]{3,}),([0-9\\-\\.]{3,}),");
+*/
+
+   if(!inFile) {
+      cerr << "Problem reading from " << filename << endl;
+   }
+   
+   //string str = "5768,\"San Luis County Regional Airport\",\"San Luis Obispo\",\"United States\",\"SBP\",\"KSBP\",35.2368,-120.642,212,-8,\"A\",\"America/Los_Angeles\"";
+   
+   //while(getline(inFile, line)) {
+      //istringstress(line);
+      
+   while(getline(inFile, line)) 
+   {
+      ss.str(line);
+      prev = index = 0;
+
+      while ((pos = line.find_first_of(delimiters, prev)) != string::npos)
+      {
+         if (pos > prev) {
+            cout << line.substr(prev, pos-prev) << endl;
+            //wordVector.push_back(line.substr(prev, pos-prev));
          }
+         prev = pos + 1;
+      }
+      if (prev < line.length()) {
+         //wordVector.push_back(line.substr(prev, std::string::npos));
+         cout << line.substr(prev, string::npos) << endl;
+      }
+      ss.flush();
+      ++numAirports;
+
+      if(numAirports > 5) {
+         break;
       }
    }
    
+   
+   cout << "Airports: " << numAirports << endl;
+   return 0;
 }
 
-
-void ParseRoutes(string filename, route_table *rt) {
-
+/*
+int ParseRoutes(string filename){//, route_table *rt) {
+   return 0;
 }
+*/
