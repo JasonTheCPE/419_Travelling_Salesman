3463702043eb58069a7f70dd2361e56c051fb11a
JasonTheCPE <jasonthecpe@gmail.com>
2016-02-09 13:45:22 -0800

    Update AirParse.cpp

--- AirParse.cpp
+++ AirParse.cpp
@@ -1,48 +1,84 @@
 
+#include <iostream>
+#include <string>
 #include "AirParse.h"
 
 using namespace std;
 
-int ParseAirports(char *filename, airport_base *ab) {
+int ParseAirports(const char* filename, airport_base *ab) {
    ifstream inFile(filename);
-   stringstream ss;
+   stringstream ss, conv;
    string line;
    string delimiters = ",\"";
    int numAirports = 0;
    int index;
    size_t prev, pos;
 
-/* //I wish we could use thee regular expression, sadly tis not to be *sadness*
+   int tempInt;
+   double tempDbl;
+
+/*
+ //I wish we could use thee regular expression, sadly tis not to be *sadness*
    regex regEx("^(\\d{1,}),\"([a-zA-Z ]{1,})\".*?\"([a-zA-Z]{3})\",.*?,([0-9\\-\\.]{3,}),([0-9\\-\\.]{3,}),");
 */
 
    if(!inFile) {
       cerr << "Problem reading from " << filename << endl;
    }
-   
-   //string str = "5768,\"San Luis County Regional Airport\",\"San Luis Obispo\",\"United States\",\"SBP\",\"KSBP\",35.2368,-120.642,212,-8,\"A\",\"America/Los_Angeles\"";
-   
-   //while(getline(inFile, line)) {
-      //istringstress(line);
       
    while(getline(inFile, line)) 
    {
+      ss.clear();
+      ss.str("");
       ss.str(line);
       prev = index = 0;
 
       while ((pos = line.find_first_of(delimiters, prev)) != string::npos)
       {
          if (pos > prev) {
-            cout << line.substr(prev, pos-prev) << endl;
+            switch(index) {
+               case ID_INDEX:
+                  //cout << "ID: " << line.substr(prev, pos - prev) << endl;
+                  conv.clear();
+                  conv.str("");
+                  conv << line.substr(prev, pos - prev);
+                  conv >> tempInt;
+                  ab->ids.push_back(tempInt);
+                  break;
+               case CITYNAME_INDEX:
+                  //cout << "CITYNAME: " << line.substr(prev, pos - prev) << endl;
+                  ab->cities.push_back(line.substr(prev, pos - prev));
+                  break;
+               case ALIAS_INDEX:
+                  //cout << "ALIAS: " << line.substr(prev, pos - prev) << endl;
+                  ab->alias.push_back(line.substr(prev, pos - prev));
+                  break;
+               case LAT_INDEX:
+                  //cout << "LAT: " << line.substr(prev, pos - prev) << endl;
+                  conv.clear();
+                  conv.str("");
+                  conv.str(line.substr(prev, pos - prev));
+                  conv >> tempDbl;
+                  ab->lats.push_back(tempDbl);
+                  break;
+               case LON_INDEX:
+                  //cout << "LON: " << line.substr(prev, pos - prev) << endl;
+                  conv.clear();
+                  conv.str("");
+                  conv.str(line.substr(prev, pos - prev));
+                  conv >> tempDbl;
+                  ab->longs.push_back(tempDbl);
+                  break;
+               default:
+                  break;
+            }
+            //cout << line.substr(prev, pos-prev) << endl;
+            ++index;
             //wordVector.push_back(line.substr(prev, pos-prev));
          }
          prev = pos + 1;
       }
-      if (prev < line.length()) {
-         //wordVector.push_back(line.substr(prev, std::string::npos));
-         cout << line.substr(prev, string::npos) << endl;
-      }
-      ss.flush();
+
       ++numAirports;
 
       if(numAirports > 5) {
@@ -50,7 +86,7 @@ int ParseAirports(char *filename, airport_base *ab) {
       }
    }
    
-   
+   ab->numAirports = numAirports;
    cout << "Airports: " << numAirports << endl;
    return 0;
 }
