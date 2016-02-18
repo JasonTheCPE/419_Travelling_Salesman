654d3458828409309c106362e3a5c33c3090c514
jasonthecpe <jasonthecpe@gmail.com>
2016-02-17 21:31:28 -0800

    proper main finally

--- Makefile
+++ Makefile
@@ -6,8 +6,7 @@
 CC= icpc
 CFLAGS= -O3 -g #-debug inline-debug-info -openmp -xHost
 LIBS=
-SRCFILES= AirParse.cpp airport.cpp route.cpp main.cpp tsp.cpp
-PARSETESTSRCFILES= AirParse.cpp tsp.cpp
+SRCFILES= AirParse.cpp tsp.cpp
 INCLUDES=
 OBJ= AirParse.o tsp.o
 
@@ -17,15 +16,12 @@ endif
 
 all: partsp
 
-mmomp: $(SRCFILES)
+partsp: $(SRCFILES) main.cpp
 	$(CC) $(CFLAGS) $(INCLUDES) -o partsp $^
-
-testparse: $(OBJ) testMain.cpp 
-	$(CC) $(CFLAGS) $(INCLUDES) -o testparse $^
 
 %.o: $.c $(PARSETESTSRCFILES)
 	$(CC) $(CFLAGS) $(INCLUDES) -o $@ $<
 	
 
 clean:
-	rm -f *.o partsp testparse result.csv *.optrpt *~
+	rm -f *.o partsp result.csv *.optrpt *~
--- main.cpp
+++ main.cpp
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <math.h>       /* sin */
+#include "airprt.h"
+#include "AirParse.h"
+#include "tsp.h"
 
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+   map<int,airport> airports;
+   map<string,city> cities;
+
+   if(argc != 3) {
+      cout << "Usage: partsp <routes.dat> <airports.dat>" << endl;
+      exit(EXIT_FAILURE);
+   }
+
+   GetAllInfo(argv[1], argv[2], &cities, &airports);
+
+/*
+   for(int i = 0; i < ab.numAirports; i++) {
+      cout << "id: " << ab.ids[i] << endl;
+      cout << "city: " << ab.cities[i] << endl;
+      cout << "alias: " << ab.alias[i] << endl;
+      cout << "lat: " << ab.lats[i] << endl;
+      cout << "lon: " << ab.longs[i] << endl << endl;
+   }
+
+   for(int i = 0; i < rt.numRoutes; i++) {
+      cout << "sourceid: " << rt.sourceID[i] << endl;
+      cout << "destid: " << rt.destID[i] << endl;
+      cout << "sourcealias: " << rt.sourceAlias[i] << endl;
+      cout << "destalias: " << rt.destAlias[i] << endl << endl;
+   }
+*/
+   return 0;
+}
